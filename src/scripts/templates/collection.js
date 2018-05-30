d5f8d40b339ee152aa267444f18448712feb2f65
Guillermo Casanova <Casanova@Guillermos-MacBook-Pro.local>
2018-05-30 00:19:49 -0400

    Making css fixes for sharing icons on page headers
     - Fixing faq and returns page headers
    
    --  Adjusting animations in collections's products
    
    - Styling footer social links and allowing them to be placed via Shopify as blocks in the footer

--- src/scripts/templates/collection.js
+++ src/scripts/templates/collection.js
@@ -37,8 +37,8 @@ theme.collection = (function() {
     TweenMax.set(selectors.collectionHeadline, {autoAlpha: 1});
 
     self.tweens.headerEntry = new TimelineMax()
-        .from(selectors.headerBackground, .6, {y: '100%'}, .4)
-        .from(selectors.collectionHeadline, .6, {y: '20px', opacity: 0}, '-=.4');
+        .from(selectors.headerBackground, .3, {y: '100%'}, .4)
+        .from(selectors.collectionHeadline, .3, {y: '20px', opacity: 0}, '-=.4');
 
 
     self.pageScene = new ScrollMagic.Scene({
@@ -56,21 +56,33 @@ theme.collection = (function() {
         
         var product = element;
 
-        console.log(product);
-
         TweenMax.set($(product).find('[data-product-img]'), {autoAlpha: 1});
 
         var productEntry = new TimelineLite()
-                .from($(product).find('[data-product-img]'), .6, {y: "101%"}, .2 + (index * .10));
+                .from($(product).find('[data-product-img]'), .3, {y: "101%"});
 
         var productScene = new ScrollMagic.Scene({
             triggerElement: product,
-            triggerHook: 'onEnter'
+            triggerHook: 'onCenter',
+            reverse: false,
+            offset: '-100'
           })
           .setTween(productEntry)
           .addTo(self.animCtrl); 
 
       });
+
+      // TweenMax.set('[data-product-img]', {autoAlpha: 1});
+
+      // var productEntry = new TimelineLite()
+      //         .from('[data-product-img]', .3, {opacity: 0});
+
+      // var productScene = new ScrollMagic.Scene({
+      //     triggerElement: ,
+      //     triggerHook: 'onEnter'
+      //   })
+      //   .setTween(productEntry)
+      //   .addTo(self.animCtrl); 
 
   }, 200); 
 
