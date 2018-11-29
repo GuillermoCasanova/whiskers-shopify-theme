e7de8bff1dd3b2f69ddb28f2e7d19b0688325579
Guillermo Casanova <Casanova@Guillermos-MacBook-Pro.local>
2018-11-29 14:03:51 -0500

    + Implementing zooming in on products for non-touch and large devices
     + Styling zooming in cursor for produc slideshow
    
     + optimizing panzoom to only launch on active image and not every image

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -35,11 +35,11 @@ theme.Product = (function() {
    */
   function Product(container) {
     this.$container = $(container);
-    
+
     var $container = (this.$container = $(container));
     var sectionId = $container.attr('data-section-id');
     var slideshow = $container.find('[data-ui-component="product-photo-slideshow"]'); 
-
+    var that = this; 
 
     //
     // Product page animations
@@ -84,6 +84,7 @@ theme.Product = (function() {
             'centerMode': false,
             'infinite': false, 
             'arrows': true, 
+            'fade': true,
             'centerPadding': '0px'
           }
         }
@@ -106,7 +107,14 @@ theme.Product = (function() {
         prevBtn.removeClass('is-hidden')
         nextBtn.removeClass('is-hidden')  
       }
+      that.initProductZoom();
     }); 
+
+    slideshow.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+      that.initProductZoom(true);
+    });
+    
+
 
     var focusProductImage = function(pIndex) {
       if(slideshow) {
@@ -159,7 +167,7 @@ theme.Product = (function() {
     }
 
     this.initAjaxCart(); 
-    this.initProductZoom();
+    this.initProductZoom(); 
 
   }
 
@@ -245,70 +253,126 @@ theme.Product = (function() {
             moneyFormat: theme.strings.moneyFormat
       });
     },
-    initProductZoom: function() {
+    initProductZoom: function(destroy) {
 
       var that = this; 
 
-
-
-      if($(window).innerWidth() > 960)  {
-         setTimeout(function() {
-
-          var isZoomed = false; 
-          $('[data-product-image]').panzoom({
-            increment: .6,
-            minScale: 1,
-            maxScale: 1.6,
-            contain: 'invert' 
-          }); 
-          
-          var zoomed = false; 
-
-          $('[data-product-image]').on('click', function() {
-
-
-            if(!zoomed) {
-
-              if( $('[data-product-image]').panzoom()) {
-                $('[data-product-image]').panzoom("reset"); 
-              }
-
-              $('[data-product-image]').panzoom("zoom");
-
-              $('[data-product-image]').on('mousemove', function(e) {
-       // var parentOffset = $('.product-photoSlideshow-slide').offset();
-       //  var relativeXPosition = (e.pageX - parentOffset.left); //offset -> method allows you to retrieve the current position of an element 'relative' to the document
-       //  var relativeYPosition = (e.pageY - parentOffset.top);
-       //    console.log(relativeXPosition)
-       //    console.log(relativeYPosition)
-       //           // console.log(e); 
-       //           var x = e.pageX - $('[data-product-image]').offset().left/$('[data-product-image]').width() * 100 + '%';
-       //           var y = e.pageY - $('[data-product-image]').offset().top/$('[data-product-image]').width() * 100 + '%';
-                
-       //            console.log(x); 
-       //            console.log(y); 
-       //           $('[data-product-image]').panzoom("pan", x * .100, y * .100); 
-
-                $('[data-product-image]').css({'transform-origin': ((e.pageX - $('.product-photoSlideshow-slide').offset().left) / $('.product-photoSlideshow-slide').width()) * 100 + '% ' + ((e.pageY - $('.product-photoSlideshow-slide').offset().top) / $('.product-photoSlideshow-slide').height()) * 100 +'%'});
-              }); 
-
-              // $('.product-photoSlideshow-slide').on('mouseout', function(e) {
-              //   $('[data-product-image]').panzoom("reset"); 
-              // }); 
-
-              zoomed = true; 
-            } else {
-              if( $('[data-product-image]').panzoom()) {
-                $('[data-product-image]').panzoom("reset"); 
-                zoomed = false; 
-              }
-              // $('[data-product-image]').panzoom("zoom");
-              // console.log($('[data-product-image]').panzoom("getTransform")); 
-            }
-          })
-        }, 1200); 
+      if(destroy && this.$activeImage.panzoom()) {
+        this.$activeImage.panzoom("reset");
+        this.$activeImage.panzoom("destroy");
+        console.log('destroy'); 
+        console.log(this.$activeImage.panzoom("instance")); 
       }
 
+      if($(window).innerWidth() > 960)  {
+
+        var selectors = {
+          activeImage: '.slick-active',
+          photoSlideshow: '.product-photoSlideshow',
+          productImage: '[data-product-image]',
+          zoomModal: '[data-zoom-modal]'
+        };
+
+        var $photoSlideshow = $(selectors.photoSlideshow); 
+        var $productImage = $(selectors.productImage);  
+        this.$activeImage =  $(selectors.activeImage).find('[data-product-image]'); 
+
+        this.$activeImage.panzoom({
+          increment: .6,
+          minScale: 1,
+          maxScale: 2.2,
+          contain: 'invert' 
+        }); 
+        
+          this.$activeImage.css('cursor','none');
+
+          $('.product-photoSlideshow-slide').append(`
+            
+            <div class="productZoomCursor">
+                <svg version="1.1" class="zoom-in" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                   viewBox="0 0 22.2 22.2" style="enable-background:new 0 0 22.2 22.2;" xml:space="preserve">
+                <g>
+                  <g>
+                    <path class="st0" d="M14.1,16c-4.4,0-8-3.6-8-8s3.6-8,8-8s8,3.6,8,8S18.5,16,14.1,16z M14.1,1c-3.9,0-7,3.1-7,7s3.1,7,7,7
+                      s7-3.1,7-7S18,1,14.1,1z"/>
+                  </g>
+                  <g>
+                    <path class="st0" d="M14.1,12.8c-0.3,0-0.5-0.2-0.5-0.5V3.8c0-0.3,0.2-0.5,0.5-0.5s0.5,0.2,0.5,0.5v8.5
+                      C14.6,12.6,14.4,12.8,14.1,12.8z"/>
+                  </g>
+                  <g>
+                    <path class="st0" d="M0.5,22.2c-0.1,0-0.3,0-0.4-0.1c-0.2-0.2-0.2-0.5,0-0.7L8.4,13c0.2-0.2,0.5-0.2,0.7,0s0.2,0.5,0,0.7l-8.3,8.3
+                      C0.8,22.2,0.6,22.2,0.5,22.2z"/>
+                  </g>
+                  <g>
+                    <path class="st0" d="M18.4,8.5H9.8C9.5,8.5,9.3,8.3,9.3,8c0-0.3,0.2-0.5,0.5-0.5h8.6c0.3,0,0.5,0.2,0.5,0.5
+                      C18.9,8.3,18.7,8.5,18.4,8.5z"/>
+                  </g>
+                </g>
+                </svg>
+
+                <svg version="1.1" class="zoom-out" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                   viewBox="0 0 22.2 22.2" style="enable-background:new 0 0 22.2 22.2;" xml:space="preserve">
+                <g>
+                  <g>
+                    <path class="st0" d="M14.1,16c-4.4,0-8-3.6-8-8s3.6-8,8-8s8,3.6,8,8S18.5,16,14.1,16z M14.1,1c-3.9,0-7,3.1-7,7s3.1,7,7,7
+                      s7-3.1,7-7S18,1,14.1,1z"/>
+                  </g>
+                  <g>
+                    <path class="st0" d="M0.5,22.2c-0.1,0-0.3,0-0.4-0.1c-0.2-0.2-0.2-0.5,0-0.7L8.4,13c0.2-0.2,0.5-0.2,0.7,0s0.2,0.5,0,0.7l-8.3,8.3
+                      C0.8,22.2,0.6,22.2,0.5,22.2z"/>
+                  </g>
+                  <g>
+                    <path class="st0" d="M18.4,8.5H9.8C9.5,8.5,9.3,8.3,9.3,8c0-0.3,0.2-0.5,0.5-0.5h8.6c0.3,0,0.5,0.2,0.5,0.5
+                      C18.9,8.3,18.7,8.5,18.4,8.5z"/>
+                  </g>
+                </g>
+                </svg>
+
+            </div>
+            `);
+
+          
+          $('.zoom-out').hide(); 
+          var zoomed = false; 
+
+          this.$activeImage.on('click', function() {
+
+            if(!zoomed) {
+              $('.zoom-in').hide(); 
+              $('.zoom-out').show(); 
+
+              if(that.$activeImage.panzoom()) {
+                that.$activeImage.panzoom("reset"); 
+              }
+              that.$activeImage.panzoom("zoom");
+              that.$activeImage.on('mousemove', function(e) {
+                that.$activeImage.css({'transform-origin': ((e.pageX - $photoSlideshow.offset().left) / $photoSlideshow.width()) * 100 + '% ' + ((e.pageY - $photoSlideshow.offset().top) / $photoSlideshow.height()) * 100 +'%'});
+              }); 
+              zoomed = true; 
+            } else {
+              if(that.$activeImage.panzoom()) {
+                that.$activeImage.panzoom("reset"); 
+                $('.zoom-in').show(); 
+                $('.zoom-out').hide(); 
+                zoomed = false; 
+              }
+            }
+          });
+
+          $photoSlideshow.on('hover', function() {
+              $(window).css('cursor','none');
+          }); 
+
+          $photoSlideshow.on('mousemove', function(event) {
+              $(window).css('cursor','none');
+              var x = event.pageX - $photoSlideshow.offset().left + 8;
+              var y = event.pageY - $photoSlideshow.offset().top + 8;
+              $('.productZoomCursor').css('left', x + 'px');
+              $('.productZoomCursor').css('top', y + 'px');
+          }); 
+
+      }
 
 
       $(selectors.zoomBtn).on('click', function(event) {
@@ -350,13 +414,9 @@ theme.Product = (function() {
           initEvents: function() {
             $image = $(selectors.zoomModalImage); 
 
-            console.log($(window).innerWidth()); 
-
             if($(window).innerWidth() > 960) {
               return false 
             } else {
-
-              console.log('small'); 
 
               var that = this; 
               $(selectors.zoomModalClose).on('click', function(){
