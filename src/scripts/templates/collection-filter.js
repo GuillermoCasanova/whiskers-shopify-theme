55bc462340d30d44f20cca6f65f28f1c1cd393be
Guillermo Casanova <Casanova@Guillermos-MacBook-Pro.local>
2019-04-08 13:06:00 -0400

    + Getting filtering system to add new results ot the top of the list of results as oppose ot the end of the array

--- src/scripts/templates/collection-filter.js
+++ src/scripts/templates/collection-filter.js
@@ -1,5 +1,11 @@
 
 
+  Array.prototype.extend = function (other_array) {
+      /* You should include a test to check whether other_array really is an array */
+      other_array.forEach(function(v) {this.push(v)}, this);
+  }
+  
+
 /**
  * Password Template Script
  * ------------------------------------------------------------------------------
@@ -18,6 +24,7 @@ theme.collectionFilter = (function() {
   };
 
 
+
   /**
   ** If there is no filtering system on this page, we do nothing  beyond this point 
   */
@@ -29,35 +36,31 @@ theme.collectionFilter = (function() {
   //This will hold our tags and other info
   */
   var tags = [],
-      collectionId = $('[data-collection-id]').data('collection-id'),
+      collection = {
+        "products": null,
+        "id": $('[data-collection-id]').data('collection-id')
+      },
       sortingState = {
         "filter": null, 
         "active": false
       },
-      productsShowing = null;  
+      filterState = {
+        "active": false,
+        "filteredProducts": null
+      };
 
-  function getFilterProducts(tags, collection){
-      $.ajax({
-          type: 'GET',
-          url: '/collections/'+ collection + '?view=json',
-          success: function(res){
-            var result = JSON.parse(res); 
-            filterProducts(result.products, tags)
-           },
-          error: function(status){
-             console.log(status);
-          }
-      })
+  function getFilterProducts(collection){
+    filterProducts(collection.products, tags); 
   }
 
   function getCollectionProducts(collection) {
     $.ajax({
         type: 'GET',
-        url: '/collections/'+ collection + '?view=json',
+        url: '/collections/'+ collection.id + '?view=json',
         success: function(res){
           var result = JSON.parse(res); 
-          productsShowing = result.products;
-          console.log(productsShowing); 
+          filterState.productsShowing = result.products;
+          collection.products = result.products; 
          },
         error: function(status){
           console.log(status);
@@ -67,29 +70,59 @@ theme.collectionFilter = (function() {
 
 
   function filterProducts(products, tags) {
+
     if(tags.length === 0 ) {
       buildFilteredProducts(products); 
-      console.log(products); 
+      filterState.productsShowing = []; 
       $('[data-results-total]').text(products.length); 
       return products
     }
 
-    var filteredProducts = products.filter(function(product){
-      for(var i = 0; i < tags.length; i++) {
-        if(product.tags.indexOf(tags[i]) > -1) {
-          return true
+
+    if(!filterState.active) {
+      var filteredProducts = products.filter(function(product){
+        for(var i = 0; i < tags.length; i++) {
+          if(product.tags.indexOf(tags[i]) > -1) {
+            return true
+          }
         }
-      }
-      return false 
-    });
+        return false 
+      });
+    } else {
+
+      var filteredProducts = products.filter(function(product){
+        for(var i = 0; i < tags.length; i++) {
+          if(product.tags.indexOf(tags[i]) > -1) {
+            return true
+          }
+        }
+        return false 
+      });
+
+      var existingFiltered = filterState.productsShowing.filter(function(product){
+        for(var i = 0; i < tags.length; i++) {
+          if(product.tags.indexOf(tags[i]) > -1) {
+            return true
+          }
+        }
+        return false 
+      });
+
+      filteredProducts = existingFiltered.concat(filteredProducts.filter(function(product) {
+        var found = JSON.stringify(existingFiltered).indexOf(JSON.stringify(product)) == - 1;
+        return found; 
+      })).reverse(); 
+    }
 
     if(sortingState.active) {
       sortProducts(filteredProducts, sortingState.option);
-      $('[data-results-total]').text(filteredProducts.length); 
     } else {
       buildFilteredProducts(filteredProducts); 
     }
+
     $('[data-results-total]').text(filteredProducts.length); 
+    filterState.active = true; 
+    filterState.productsShowing = filteredProducts; 
     return filteredProducts;
   }
 
@@ -140,9 +173,10 @@ theme.collectionFilter = (function() {
   function clearAllFilters() {
     tags = []; 
     $(selectors.collectionFilterOption).removeAttr('checked');
-    getFilterProducts(tags, collectionId);
+    getFilterProducts(collection);
   }
 
+  
   function sortProducts(products, sortOption) {
     var sortedProducts = null; 
 
@@ -159,49 +193,59 @@ theme.collectionFilter = (function() {
     }
 
     var sortedProducts = products.sort(dynamicSort(sortOption));
-    console.log(sortedProducts);
     buildFilteredProducts(sortedProducts);  
     sortingState.active = true;
     sortingState.option = sortOption;
   }
 
 
-  getCollectionProducts(collectionId); 
 
 
-  $(selectors.collectionFilterOption).on('change', function(){
-    console.log('change'); 
+  //
+  // Sets up the sorting drop down 
+  //
+   $(selectors.collectionSortOption).on('change', function() {
+    var sortOption = this.value; 
+    sortProducts(filterState.productsShowing, sortOption); 
+   }); 
+
+
+   $(selectors.collectionFilterOption).on('change', function(){
       var tag = $(this).val()
       if(tags.indexOf(tag) != -1) {
         _.pull(tags, tag); 
       } else {
         tags.push(tag); 
       }
-      getFilterProducts(tags, collectionId);
-  });
-
-
-   $('[data-clear-filters]').on('click', function() {
-      clearAllFilters(); 
+      getFilterProducts(collection);
    });
 
-   $(selectors.collectionSortOption).on('change', function() {
-    var sortOption = this.value; 
-    console.log(sortOption); 
-    sortProducts(productsShowing, sortOption); 
-   }); 
-
+  //
+  // Sets up the filtering elements 
+  //
   if($(selectors.collectionFiltersToggle).length > 0 ) {
 
-  var $filterToggle = $(selectors.collectionFiltersToggle); 
+    var $filterToggle = $(selectors.collectionFiltersToggle); 
 
-  function toggleFiltersMenu() {
-    $('[data-filters-off-canvas]').toggleClass('is-hidden'); 
-    $('body').toggleClass('filter-off-canvas-open');
-  }
+    function toggleFiltersMenu() {
+      $('[data-filters-off-canvas]').toggleClass('is-hidden'); 
+      $('body').toggleClass('filter-off-canvas-open');
+    }
 
-  $filterToggle.on('click', toggleFiltersMenu); 
+    $filterToggle.on('click', toggleFiltersMenu); 
   
+    $('[data-clear-filters]').on('click', function() {
+      clearAllFilters(); 
+    });
+
   }
+
+
+  //
+  // Gets the collection's products and stores it so we dont need to make more calls to get all products for it
+  //
+  getCollectionProducts(collection); 
+
+
 })();
 
