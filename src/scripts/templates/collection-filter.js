edc77eb39afc5f2661696e068ebf38ddd93797d6
Guillermo Casanova <Casanova@Guillermos-MacBook-Pro.local>
2019-04-08 14:46:49 -0400

    + Setting up filtering system to show filtering tags above the results for easy toggling of tags  and for user to know what they are filtering by so far
     + Adjuting padding on page headers
    
     + Adjusting CSS for collection descriptions to adjust for change in HTML of collection pages

--- src/scripts/templates/collection-filter.js
+++ src/scripts/templates/collection-filter.js
@@ -20,7 +20,9 @@ theme.collectionFilter = (function() {
     collectionMenu: '[data-ui-component="collection-menu"]',
     collectionFiltersToggle: '[data-filter-toggle]',
     collectionFilterOption: '[data-filter-option]',
-    collectionSortOption: '[data-sort-option]'
+    collectionSortOption: '[data-sort-option]',
+    collectionDescription: '[data-collection-description]',
+    activeTags: '[data-active-tags-container]'
   };
 
 
@@ -73,7 +75,9 @@ theme.collectionFilter = (function() {
 
     if(tags.length === 0 ) {
       buildFilteredProducts(products); 
+      buildActiveTags(tags);
       filterState.productsShowing = []; 
+      filterState.active = false; 
       $('[data-results-total]').text(products.length); 
       return products
     }
@@ -108,6 +112,8 @@ theme.collectionFilter = (function() {
         return false 
       });
 
+      existingFiltered.reverse(); 
+
       filteredProducts = existingFiltered.concat(filteredProducts.filter(function(product) {
         var found = JSON.stringify(existingFiltered).indexOf(JSON.stringify(product)) == - 1;
         return found; 
@@ -123,6 +129,7 @@ theme.collectionFilter = (function() {
     $('[data-results-total]').text(filteredProducts.length); 
     filterState.active = true; 
     filterState.productsShowing = filteredProducts; 
+    buildActiveTags(tags); 
     return filteredProducts;
   }
 
@@ -173,11 +180,12 @@ theme.collectionFilter = (function() {
   function clearAllFilters() {
     tags = []; 
     $(selectors.collectionFilterOption).removeAttr('checked');
+    buildActiveTags(tags);
     getFilterProducts(collection);
   }
 
-  
-  function sortProducts(products, sortOption) {
+
+  function sortProducts(pProducts, pSortOption) {
     var sortedProducts = null; 
 
     function dynamicSort(property) {
@@ -192,33 +200,62 @@ theme.collectionFilter = (function() {
       }
     }
 
-    var sortedProducts = products.sort(dynamicSort(sortOption));
+    var sortedProducts = pProduts.sort(dynamicSort(pSortOption));
     buildFilteredProducts(sortedProducts);  
     sortingState.active = true;
-    sortingState.option = sortOption;
+    sortingState.option = pSortOption;
   }
 
 
+  function buildActiveTags(pTags) {
+    var $tagsContainer = $(selectors.activeTags); 
+    var $collectionDescription = $(selectors.collectionDescription); 
 
+    $tagsContainer.empty(); 
+    $collectionDescription.hide(); 
 
-  //
-  // Sets up the sorting drop down 
-  //
-   $(selectors.collectionSortOption).on('change', function() {
-    var sortOption = this.value; 
-    sortProducts(filterState.productsShowing, sortOption); 
-   }); 
+    if(pTags.length == 0 || !pTags) {
+      $collectionDescription.show(); 
+      return  
+    }
 
+    var labelTags = [],
+    data = {}, 
+    source = $("#tags-template").html(),
+    template = Handlebars.compile(source);
 
-   $(selectors.collectionFilterOption).on('change', function(){
-      var tag = $(this).val()
-      if(tags.indexOf(tag) != -1) {
-        _.pull(tags, tag); 
-      } else {
-        tags.push(tag); 
+    pTags.forEach(function(pTagLabel) {
+      var tagName = pTagLabel.replace("color-", "");
+      var tag = {};
+      tag.name = tagName; 
+      tag.label = pTagLabel; 
+
+      labelTags.push(tag); 
+    });
+
+    data = {
+      tag: labelTags
+    }
+
+    $tagsContainer.append(template(data)); 
+
+    var $activeTag = $('[data-active-tag]');
+
+    $activeTag.on('click', function() {
+      var tagLabel = $(this).data('label');
+      $(this).remove(); 
+      removeActiveTag(tagLabel);
+    }); 
+  }
+
+  function removeActiveTag(pTag) {
+    for(var i = 0; i < tags.length; i++) {
+      if(pTag === tags[i]) {
+        tags.splice(i, 1); 
       }
-      getFilterProducts(collection);
-   });
+    }
+    filterProducts(collection.products, tags); 
+  }
 
   //
   // Sets up the filtering elements 
@@ -241,10 +278,31 @@ theme.collectionFilter = (function() {
   }
 
 
+   $(selectors.collectionFilterOption).on('change', function(){
+      var tag = $(this).val()
+      if(tags.indexOf(tag) != -1) {
+        _.pull(tags, tag); 
+      } else {
+        tags.push(tag); 
+      }
+      getFilterProducts(collection);
+   });
+
+
   //
   // Gets the collection's products and stores it so we dont need to make more calls to get all products for it
   //
   getCollectionProducts(collection); 
+
+
+  //
+  // Sets up the sorting drop down 
+  //
+ // $(selectors.collectionSortOption).on('change', function() {
+ //  var sortOption = this.value; 
+ //  sortProducts(filterState.productsShowing, sortOption); 
+ // }); 
+
 
 
 })();
